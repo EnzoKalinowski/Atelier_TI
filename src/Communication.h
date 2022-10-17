a61fd1a04fa07994006f028276fec58f543df3d6
EnzoKalinowski <enzo.kalinowski@gmail.com>
2022-10-17 10:26:24 +0200

    header guards

---
+++ src/Communication.h
@@ -0,0 +1,5 @@
+#ifndef __COMMUNICATION_H__
+#define __COMMUNICATION_H__
+
+
+#endif // __COMMUNICATION_H__
---
+++ src/Tracking.h
@@ -0,0 +1,12 @@
+#ifndef __TRACKING_H__
+#define __TRACKING_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "./NRC/def.h"
+#include "./NRC/nrio.h"
+#include "./NRC/nrarith.h"
+#include "./NRC/nralloc.h"
+
+#endif // __TRACKING_H__
