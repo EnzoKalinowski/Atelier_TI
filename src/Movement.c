2fa86669e01a968269c25ff7035148769560c771
ewenn <samson.ewenn@hotmail.fr>
2022-10-19 11:11:31 +0200

    modif du main evec libe de memo, supp des coms et modif de la fct de vects

--- src/Movement.c
+++ src/Movement.c
@@ -6,15 +6,21 @@ void ** Vecteur (byte **imgT, byte **imgTplusUn, int *x, int *y, long nrl, long 
 	int Y = (ncl-nch)/2;
 	int xplus =0;
 	int yplus =0;
+	//printf("avant la lecture des image et l'insertion dans la table d'accumulation\n");
 	for (int i= nrl; i< nrh; i++){
 		for(int j= ncl; j<nch; j++){
 			if(imgT[i][j]==255){
+				//printf("1ER IF\n");
 				int vect = 1000;	
 				for (int iplus= nrl; iplus< nrh; iplus++){
 					for(int jplus= ncl; jplus<nch; jplus++){
 						if(imgTplusUn[iplus][jplus]==255){
+							//printf("2ND IF\n");
+							//printf("iplus = %d/%d, jplus = %d/%d \n",iplus,nrh,jplus,nch);
 							int vectcalcule = abs(iplus-i)+(jplus-j);
+							//printf("APRES VECTCALCULE!!!\n");
 							if (vectcalcule<vect){
+								//printf("3EME IF\n");
 								vect = vectcalcule;
 								xplus = iplus;
 								yplus = jplus;
@@ -23,9 +29,12 @@ void ** Vecteur (byte **imgT, byte **imgTplusUn, int *x, int *y, long nrl, long 
 					}
 				}
 			}
-			tabaccu[i+xplus][j+yplus] ++; 
+			//printf("Avant de remplir tabaccu\n");
+			tabaccu[xplus][yplus] ++; 
+			//printf("Apres avoir rempli tabaccu\n");
 		}
 	}
+	//printf("apres avoir rempli la table d'accumulation\n");
 	int maxaccu = 0;
 	for (int i= nrl; i< nrh; i++){
 		for(int j= ncl; j<nch; j++){
@@ -34,9 +43,13 @@ void ** Vecteur (byte **imgT, byte **imgTplusUn, int *x, int *y, long nrl, long 
 				xplus = i;
 				yplus = j;
 			}
+			//printf("%d",tabaccu[i][j]);
 		}
+		//printf("\n");
 	}
+	//printf("apres avoir recupere le vecteur\n");
 	x = &xplus;
 	y = &yplus;
+	//printf("fin de la fonction \n");
 	free_imatrix (tabaccu, nrl, nrh, ncl, nch);
 }
