0f6c372fbf229adf16392b0be00687facdcd340e
ewenn <samson.ewenn@hotmail.fr>
2022-10-19 13:01:10 +0200

    enregistrement des images pour debug

--- src/main.c
+++ src/main.c
@@ -33,15 +33,18 @@ int main(){
     //printf("On a ouvert la seconde img\n");
     byte **ImgTGris;
     ImgTGris=bmatrix(nrl, nrh, ncl, nch);
+    
     //printf("On convertis la premire image en nuance de gris \n");
     //convert_rgb8_to_byte(rgb8 **I, byte **B, long nrl, long nrh, long ncl, long nch);
     convert_rgb8_to_byte(ImgT, ImgTGris, nrl, nrh, ncl, nch);
+    SavePGM_bmatrix(ImgTGris,nrl, nrh, ncl, nch,"ImgTGris.pgm");
     byte **ImgTPlusUnGris;
     ImgTPlusUnGris=bmatrix(nrl, nrh, ncl, nch);
-
+    
+    
     //printf("On convertis la seconde image en nuance de gris \n");
     convert_rgb8_to_byte(ImgTPlusUn, ImgTPlusUnGris, nrl, nrh, ncl, nch);
-
+    SavePGM_bmatrix(ImgTGris,nrl, nrh, ncl, nch,"ImgTPlusUnGris.pgm");
 
     int filter_size = size;
     float lambda = sigma;
@@ -54,8 +57,10 @@ int main(){
     //void convert_dmatrix_bmatrix(double **D, byte **B, long nrl, long nrh, long ncl, long nch)
     //printf("On converti la premiere Img \n");
     convert_dmatrix_bmatrix(ImgTHarris, ImgTGris, nrl, nrh, ncl, nch);
+    SavePGM_bmatrix(ImgTGris,nrl, nrh, ncl, nch,"ImgTHarris.pgm");
     //printf("On converti la seconde Img \n");
     convert_dmatrix_bmatrix(ImgTPlusUnHarris, ImgTPlusUnGris, nrl, nrh, ncl, nch);
+    SavePGM_bmatrix(ImgTPlusUnGris,nrl, nrh, ncl, nch,"ImgTPlusUnHarris.pgm");
 
 
     //void binarize(byte **I, byte **B, int treshold, long nrl, long nrh, long ncl, long nch)
